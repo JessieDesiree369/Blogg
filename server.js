6bb23b61dd1f539f8edb290f87abc2e81fda46e4
Amalthea <rainey22598@gmail.com>
2022-11-11 19:36:30 -0500

    troubleshooting

---
+++ server.js
@@ -0,0 +1,28 @@
+// import express from 'express';
+// import { engine } from 'express-handlebars';
+
+//Loads the express module
+const express = require('express');
+//Creates our express server
+const app = express();
+const port = 3003;
+
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
+// app.engine('handlebars', handlebars({
+// layoutsDir: __dirname + '/views/layouts',
+// }));
+
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))
+//Sets a basic route
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('main', {layout : 'index'});
+    });
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
