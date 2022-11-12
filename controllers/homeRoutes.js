236a6ec13fc1cd8f384d08df98864850bdffc23e
Amalthea <rainey22598@gmail.com>
2022-11-12 03:18:01 -0500

    more troubleshootin'

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    // Add a comment describing the purpose of the render method
-    // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-    res.render('home');
+
+    res.render('index');
   });
 
   module.exports = router;
